ba0bb150ba4233877ebadb1759b5a08f2c0db987
brevre <brendamakena09@gmail.com>
2023-10-14 03:56:38 +0200

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+document.addEventListener('DOMContentLoaded', function () {
+  const form = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
+
+  form.addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent the form from submitting and page reloading
+
+    const taskInput = document.getElementById('new-task-description');
+    const taskText = taskInput.value;
+
+    if (taskText) { // Ensure the input is not empty
+      const taskItem = document.createElement('li');
+      taskItem.textContent = taskText;
+
+      // Optionally add a delete button or other functionality to the taskItem
+
+      taskList.appendChild(taskItem); // Append the task to the list
+
+      taskInput.value = ''; // Clear the input field
+      taskItem.innerHTML = `${taskText} <button class="delete-button">Delete</button>`;
+const deleteButton = taskItem.querySelector('.delete-button');
+deleteButton.addEventListener('click', function () {
+  taskList.removeChild(taskItem);
+});
+
+    }
+  });
+});
+
